34a473128de05df25a94aa408cc7c90a3d1e2cfa
Vasskan <vasskan@ukr.net>
2016-11-23 22:39:32 +0200

    17

---
+++ 17.php
@@ -0,0 +1,7 @@
+<?php
+$month="<b>November</b>";
+$arr = array ('January', 'February', 'March', 'April', 'May', 'June', 'Luly', 'August', 'September', 'October', 'November', 'December');
+$arr[10]=$month;
+foreach ($arr as $value) {
+    echo "$value <br>";
+    }
